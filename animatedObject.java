f09daeccd0b784db583d2d044693002681c6ba8e
gamerboy69 <50874608+gamerboy69@users.noreply.github.com>
2019-05-21 09:22:29 -0400

    Create animatedObject.java

---
+++ animatedObject.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package invadersapplet;
+import java.awt.*;
+import java.applet.*;
+/**
+ *
+ * @author matthewdaly
+ */
+public class animatedObject {
+    private Graphics g;
+    private int posX; private int posY; private int width; 
+    private int height; 
+    private int speedX; private int speedY;
+    
+    public animatedObject(Graphics graph) {
+        g = graph;
+    }
+    
+    public void setup(int x, int y, int width_, int height_, int speedX_, int speedY_) {
+        // speed in pixels per second
+        posX = x; posY = y; width = width_; height = height_; speedX = speedX_; speedY = speedY_;
+    }
+    
+    public void setVelocity(int speedX, int speedY) {
+        this.speedX = speedX; this.speedY = speedY;
+    }
+    
+    public void draw() {
+        g.drawRect(posX, posY, width, height);
+    }
+    
+    public void update() {
+        g.clearRect(posX, posY, width + 10, height + 10);
+        posX += speedX;
+        posY += speedY;
+        g.drawRect(posX, posY, width, height);
+    }
+    
+    private boolean pointIn(int x, int y) {
+        // is x in boundaries?
+        if (posX <= x && x <= posX) {
+            if (posY <= y && y <= posY) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean rectIn(int x, int y, int rectWidth, int rectHeight) {
+        return true;
+    }
+    
+}
